b04cd74d54ed7e28e1b54476d1c0cb3fc3bf97dc
willxm <chenweimin74@gmail.com>
2017-05-06 14:30:13 +0800

    opt

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"unicode"
+
+	"strings"
 
 	"github.com/huichen/sego"
 )
 
 func readFile(path string) []byte {
-	fi, err := os.Open("news.txt")
+	fi, err := os.Open(path)
 	if err != nil {
 		panic(err)
 	}
@@ -24,18 +25,38 @@ func readFile(path string) []byte {
 
 func main() {
 	var segmenter sego.Segmenter
-	segmenter.LoadDictionary("/Users/willxm/go/src/github.com/huichen/sego/data/dictionary.txt")
+	//load dictionary
+	segmenter.LoadDictionary("data/dictionary.txt")
+	//load news data
+	text := readFile("data/news.txt")
+	//load stopword
+	stopwords := readFile("data/stopwords.txt")
+	sws := strings.Split(string(stopwords), "\n")
 
-	text := readFile("news.txt")
 	segments := segmenter.Segment(text)
 
 	texts := sego.SegmentsToSlice(segments, false)
 
-	for _, v := range texts {
-		if !unicode.IsLetter([]rune(v)[0]) {
+	fo, err := os.Create("sego.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fo.Close()
 
-		} else {
-			fmt.Println(v)
+	l := len(sws)
+
+	for _, v := range texts {
+		var i int
+		i = 0
+		if unicode.IsLetter([]rune(v)[0]) {
+			for _, vs := range sws {
+				if v != vs {
+					i++
+				}
+			}
+		}
+		if i == l {
+			fo.WriteString(v + "\t")
 		}
 	}
 }
